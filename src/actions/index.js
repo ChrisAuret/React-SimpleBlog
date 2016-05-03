2613abeee361b92c997e1f18c474452be8e93c49
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-03 21:06:35 +1000

    Wire up action to container

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 
-const ROOT_URL = "http://reduxplog/herokyaoo.com/api";
-const API_KEY = "?KEY=klO32390lk";
+const ROOT_URL = "http://reduxblog.herokuapp.com/api";
+const API_KEY = "?key=klO32390lk";
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {  
-    return (
-        <div>List of blog posts </div>
-    );
-};
+class PostsIndex extends Component {
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+    
+    render() {
+        return (
+            <div>List of blog posts</div>
+        );   
+    }
+}
+
+export default connect(null, {fetchPosts: fetchPosts })(PostsIndex);
+
+// Shortcut for:
+// function  mapDispatchToProps(dispatch) {
+//     return bindActionCreators({ fetchPosts }, dispatch);
+// }
+// export default connect(null, mapDispatchToProps)(PostsIndex);
+// ==> export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
+// ==> export default connect(null, { fetchPosts })(PostsIndex);
