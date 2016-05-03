a1214d7c4fcf9f17bfe340c99e96daa899e7f951
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-03 21:22:39 +1000

    Add New Post component and routing

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {  
+    render() {
+        return (
+            <div>Create form</div>
+        );   
+    };
+}
+
+export default PostsNew;
+
+//export default connect(null, {fetchPosts: fetchPosts })(PostsNew);
