820a42a0177c9e01e10388f384c91945864ce081
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-04 15:29:18 +1000

    Add more validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -10,22 +10,28 @@ class PostsNew extends Component {
             <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create A New Post</h3>
                 
-                <div className="form-group">
+                <div className={ title.touched && title.invalid ? 'has-danger' : ''   } >
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
                     <div className="text-help">
-                    {title.touched ? title.error : ""}
+                        {title.touched ? title.error : ""}
                     </div>
                 </div>
                 
-                <div className="form-group">
+                <div className={ categories.touched && categories.invalid ? 'has-danger' : ''   } >
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories}  />
+                    <div className="text-help">
+                        {categories.touched ? categories.error : ""}
+                    </div>
                 </div>
                 
-                <div className="form-group">
+                <div className={ content.touched && content.invalid ? 'has-danger' : ''   } >
                     <label>Content</label>
                     <input type="text" className="form-control" {...content} />
+                    <div className="text-help">
+                        {content.touched ? content.error : ""}
+                    </div>
                 </div>
                 
                 <button type="submit" className="btn btn-primary">Submit</button>      
@@ -42,11 +48,11 @@ function validate(values){
     }
     
     if(!values.categories){
-        errors.title = "Please enter a category";
+        errors.categories = "Please enter a category";
     }
     
     if(!values.content){
-        errors.title = "Please enter some content";
+        errors.content = "Please enter some content";
     }
     
     return errors;
@@ -61,7 +67,8 @@ function validate(values){
 // 3rd arg is mapDispatchToProps
 export default reduxForm({
     form: 'PostsNewForm',
-    fields: ['title', 'categories', 'content']   
+    fields: ['title', 'categories', 'content'],
+    validate
 }, null, {createPost})(PostsNew);
 
 //export default connect(null, {fetchPosts: fetchPosts })(PostsNew);
